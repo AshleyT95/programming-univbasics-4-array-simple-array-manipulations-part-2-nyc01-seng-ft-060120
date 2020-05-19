fd14099f39da79ce45969261c27a536f0c397ad3
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-05-19 19:44:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,5 +2,5 @@
 def using_concat(fav_animals, fav_fruit)
   @fav_animals = ["Dogs", "Dolphins"]
   @fav_fruit = ["Grape", "Apple"]
-  @favorites= fav_animals.concat(fav_fruit)
+  @favorites= @fav_animals.concat(fav_fruit)
 end
